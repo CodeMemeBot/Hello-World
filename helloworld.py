5062ad0970d45c7ec5bc8b6f35e2a719a5614c7a
CodeMemeBot <67338812+CodeMemeBot@users.noreply.github.com>
2020-06-23 23:11:59 -0300

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,86 @@
+import math
+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=0
+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name1=0
+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name2=0
+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name3=0
+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name4=0
+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name5=0
+def fukkk():
+ global The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==0:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=1
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==1:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=2
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==2:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=3
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==3:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=4
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==4:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=5
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==5:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=6
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==6:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=7
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==7:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=8
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==8:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=9
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==9:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=10
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==10:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=11
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==11:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=12
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==12:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=13
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==13:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=14
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==14:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=15
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==15:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=16
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==16:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=17
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==17:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=18
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==18:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=19
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==19:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=20
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==20:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=21
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==21:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=22
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==22:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=23
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==23:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=24
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==24:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=25
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==25:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=26
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==26:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=27
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==27:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=28
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==28:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=29
+ if The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name==29:
+  The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name=30
+ return The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name
+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name1=fukkk()
+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name2=fukkk()
+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name3=fukkk()
+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name4=fukkk()
+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name5=fukkk()
+Well_at_least_this_variable_is_not_that_long_right=The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name1+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name2+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name3+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name4+The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name5+20
+
+def printkkk(strin):
+ print(strin[0]+"\r"+strin[1]+"\r"+strin[2]+"\r"+strin[3]+"\r"+strin[4]+"\r"+strin[5]+"\r"+chr(int((math.sin(math.pi/2)*8)**2+8))+chr((The_Variable_That_Keeps_Adding_Up_Wait_Is_This_A_Long_Variable_Name5/3)**2+1) +
+	   chr(((3**2*3)*2)*2)+chr(int("6C",16))+chr(int(str(int(math.sin(math.pi/2)))+str(int(math.sin(math.pi/2)))+str(int(math.sin(math.pi/2)))))+chr((2**2**2)*2)+ 
+	   chr(int( math.acos(math.cos(math.pi/2))-math.asin(math.sin(math.pi/2))+int(str(2**2*2)+"7")))+chr(int(str(int(pow(1.0,0)))+str(int(pow(1.0,22.435)))+str(int(pow(1.0,69.6969420420)))))+
+	   chr(int(math.sqrt(math.sqrt(168896016))))+chr(int(0.000054*2000000))+ 
+	   "d"   ) #enough is enough
+
+if format(Well_at_least_this_variable_is_not_that_long_right,"x")=="aa":
+ printkkk("bucetacaraikkk")
